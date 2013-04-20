2704b9fdc318537b628efdb089d68046a3a9f551
Andre Schweiger <andre.spiele@yahoo.de>
2013-04-20 10:02:42 +0200

    Add cast on Location

--- DvZ/src/me/andre111/dvz/item/spell/ItemDamage.java
+++ DvZ/src/me/andre111/dvz/item/spell/ItemDamage.java
@@ -1,6 +1,10 @@
 package me.andre111.dvz.item.spell;
 
+import java.util.ArrayList;
+
+import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import me.andre111.dvz.Game;
@@ -10,10 +14,13 @@ public class ItemDamage extends ItemSpell {
 	private boolean self = true;
 	private int damage = 4;
 	
+	private double range;
+	
 	@Override
 	public void setCastVar(int id, double var) {
 		if(id==0) self = var==1;
 		else if(id==1) damage = (int) Math.round(var);
+		else if(id==2) range = var;
 	}
 	
 	@Override
@@ -35,6 +42,29 @@ public class ItemDamage extends ItemSpell {
 			return castIntern(game, player, player);
 		else
 			return castIntern(game, target, player);
+	}
+	
+	@Override
+	public boolean cast(Game game, Player player, Location loc) {
+		if(self) return false;
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		for(Entity e : loc.getWorld().getEntities()) {
+			if(e instanceof Player) {
+				if(e.getLocation().distanceSquared(loc)<=range*range) {
+					players.add((Player) e);
+				}
+			}
+		}
+		
+		for(Player p : players) {
+			castIntern(game, p, player);
+		}
+		
+		if(players.size()>0) {
+			return true;
+		}
+		return false;
 	}
 	
 	private boolean castIntern(Game game, Player player, Player source) {
