ad7a0c34e79eedcd6c733a4a5db74f1f6f68b908
Andre Schweiger <andre.spiele@yahoo.de>
2013-06-12 14:38:40 +0200

    Add Blockbreak blacklist/change system
    Signed-off-by: Andre Schweiger <andre.spiele@yahoo.de>

--- DvZ/src/me/andre111/dvz/BlockManager.java
+++ DvZ/src/me/andre111/dvz/BlockManager.java
@@ -15,29 +15,108 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class BlockManager {
-	private static ArrayList<String> gameType1 = new ArrayList<String>();
-	private static ArrayList<String> gameType2 = new ArrayList<String>();
+	private static ArrayList<String> gameType1_dwarf = new ArrayList<String>();
+	private static ArrayList<String> gameType1_monster = new ArrayList<String>();
+	private static ArrayList<String> blacklist1_dwarf = new ArrayList<String>();
+	private static ArrayList<String> blacklist1_monster = new ArrayList<String>();
+	private static ArrayList<String> changeBreak1_dwarf = new ArrayList<String>();
+	private static ArrayList<String> changeBreak1_monster = new ArrayList<String>();
+	private static boolean infiniteCake1_dwarf;
+	private static boolean infiniteCake1_monster;
 	
-	private static boolean infiniteCake1;
-	private static boolean infiniteCake2;
+	private static ArrayList<String> gameType2_dwarf = new ArrayList<String>();
+	private static ArrayList<String> gameType2_monster = new ArrayList<String>();
+	private static ArrayList<String> blacklist2_dwarf = new ArrayList<String>();
+	private static ArrayList<String> blacklist2_monster = new ArrayList<String>();
+	private static ArrayList<String> changeBreak2_dwarf = new ArrayList<String>();
+	private static ArrayList<String> changeBreak2_monster = new ArrayList<String>();
+	private static boolean infiniteCake2_dwarf;
+	private static boolean infiniteCake2_monster;
 
 	public static void loadConfig() {
-		 gameType1.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType1"));
-		 infiniteCake1 = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType1");
-		 
-		 gameType2.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType2"));
-		 infiniteCake2 = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType2");
+		gameType1_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType1.dwarves"));
+		gameType1_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType1.monsters"));
+		blacklist1_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakBlackList.gameType1.dwarves"));
+		blacklist1_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakBlackList.gameType1.monsters"));
+		changeBreak1_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakChangeBlock.gameType1.dwarves"));
+		changeBreak1_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakChangeBlock.gameType1.monsters"));
+		infiniteCake1_dwarf = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType1.dwarves");
+		infiniteCake1_monster = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType1.monsters");
+		
+		gameType2_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType2.dwarves"));
+		gameType2_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.drop.gameType2.monsters"));
+		blacklist2_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakBlackList.gameType2.dwarves"));
+		blacklist2_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakBlackList.gameType2.monsters"));
+		changeBreak2_dwarf.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakChangeBlock.gameType2.dwarves"));
+		changeBreak2_monster.addAll(ConfigManager.getBlockFile().getStringList("blocks.breakChangeBlock.gameType2.monsters"));
+		infiniteCake2_dwarf = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType2.dwarves");
+		infiniteCake2_monster = ConfigManager.getBlockFile().getBoolean("blocks.infiniteCake.gameType2.monsters");
 	}
 	
 	public static void onBlockBreak(BlockBreakEvent event) {
+		if(event.isCancelled()) return;
+		
+		int blockID = event.getBlock().getTypeId();
+		byte data = event.getBlock().getData();
+		
 		Player player = event.getPlayer();
 		Game game = DvZ.instance.getPlayerGame(player.getName());
 		
 		if(game!=null) {
 			int gType = game.getGameType();
-			if(gType==1) changeDrops(event, gameType1);
-			else if(gType==2) changeDrops(event, gameType2);
+			//Blockbreak blacklist
+			if(game.isDwarf(player.getName(), true)) {
+				if(gType==1) if(isBlackListed(blockID, data, blacklist1_dwarf)) {
+					event.setCancelled(true);
+					return;
+				} else 
+				if(gType==2) if(isBlackListed(blockID, data, blacklist2_dwarf)) {
+					event.setCancelled(true);
+					return;
+				}
+			} else {
+				if(gType==1) if(isBlackListed(blockID, data, blacklist1_monster)) {
+					event.setCancelled(true);
+					return;
+				} else 
+				if(gType==2) if(isBlackListed(blockID, data, blacklist2_monster)) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+			
+			//change drops
+			if(game.isDwarf(player.getName(), true)) {
+				if(gType==1) changeDrops(event, gameType1_dwarf);
+				else if(gType==2) changeDrops(event, gameType2_dwarf);
+			} else {
+				if(gType==1) changeDrops(event, gameType1_monster);
+				else if(gType==2) changeDrops(event, gameType2_monster);
+			}
+			
+			//change block on break
+			if(game.isDwarf(player.getName(), true)) {
+				if(gType==1) changeBreak(event, blockID, data, changeBreak1_dwarf);
+				else if(gType==2) changeBreak(event, blockID, data, changeBreak2_dwarf);
+			} else {
+				if(gType==1) changeBreak(event, blockID, data, changeBreak1_monster);
+				else if(gType==2) changeBreak(event, blockID, data, changeBreak2_monster);
+			}
 		}
+	}
+	
+	private static boolean isBlackListed(int blockID, byte data, ArrayList<String> blacklist) {
+		for(String st : blacklist) {
+			//check if it is the right block
+			String[] binfo = st.split(":");
+			
+			if(Integer.parseInt(binfo[0])==blockID) {
+				if(Integer.parseInt(binfo[1])==data) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	private static void changeDrops(BlockBreakEvent event, ArrayList<String> drop) {
@@ -70,6 +149,26 @@ public class BlockManager {
 		}
 	}
 	
+	private static void changeBreak(BlockBreakEvent event, int blockID, byte data, ArrayList<String> drop) {
+		for(String st : drop) {
+			String[] split = st.split(";");
+			
+			//check if it is the right block
+			String[] binfo = split[0].split(":");
+			String[] newBlock = split[1].split(":");
+			
+			
+			if(Integer.parseInt(binfo[0])==blockID) {
+				if(Integer.parseInt(binfo[1])==data) {
+					event.setCancelled(true);
+					
+					Block b = event.getBlock();
+					b.setTypeIdAndData(Integer.parseInt(newBlock[0]), (byte)Integer.parseInt(newBlock[1]), true);
+				}
+			}
+		}
+	}
+	
 	//infinite cake
 	public static void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
@@ -82,8 +181,18 @@ public class BlockManager {
 	}
 	
 	private static void cakeEaten(PlayerInteractEvent event, int gameType) {
-		if(gameType==1 && !infiniteCake1) return;
-		if(gameType==2 && !infiniteCake2) return;
+		Player player = event.getPlayer();
+		Game game = DvZ.instance.getPlayerGame(player.getName());
+		
+		if(game!=null) {
+			if(game.isDwarf(player.getName(), true)) {
+				if(gameType==1 && !infiniteCake1_dwarf) return;
+				if(gameType==2 && !infiniteCake2_dwarf) return;
+			} else {
+				if(gameType==1 && !infiniteCake1_monster) return;
+				if(gameType==2 && !infiniteCake2_monster) return;
+			}
+		}
 		
 		if(event.getAction()==Action.RIGHT_CLICK_BLOCK) {
 			Block block = event.getClickedBlock();
