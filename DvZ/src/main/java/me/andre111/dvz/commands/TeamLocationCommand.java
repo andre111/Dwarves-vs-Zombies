3c3035a1a76eeeae06cf073a94bd7aec0a913a21
Andre Schweiger <andre.spiele@yahoo.de>
2014-05-01 21:15:42 +0200

    Fixing
    Signed-off-by: Andre Schweiger <andre.spiele@yahoo.de>

--- DvZ/src/main/java/me/andre111/dvz/commands/TeamLocationCommand.java
+++ DvZ/src/main/java/me/andre111/dvz/commands/TeamLocationCommand.java
@@ -1,6 +1,7 @@
 package me.andre111.dvz.commands;
 
 import java.io.File;
+import java.io.IOException;
 
 import me.andre111.dvz.DvZ;
 import me.andre111.dvz.Game;
@@ -53,13 +54,24 @@ public class TeamLocationCommand extends DvZCommand {
 		}
 		
 		String path = Bukkit.getServer().getWorldContainer().getPath()+"/"+player.getWorld().getName()+"/dvz/locations.yml";
-		DVZFileConfiguration save = DVZFileConfiguration.loadConfiguration(new File(path));
+		File file = new File(path);
+		if(!file.getParentFile().exists()) {
+			file.getParentFile().mkdir();
+		}
+		DVZFileConfiguration save = DVZFileConfiguration.loadConfiguration(file);
 		
 		save.set(team+"."+loc+".x", location.getX());
 		save.set(team+"."+loc+".y", location.getY());
 		save.set(team+"."+loc+".z", location.getZ());
 		save.set(team+"."+loc+".yaw", location.getYaw());
 		save.set(team+"."+loc+".pitch", location.getPitch());
+		
+		try {
+			save.save(new File(path));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		//TODO - set location in active game
 		if(gameID!=-1) {
